d916d08b2fd151e9f9414205540473ef6eff7d7d
Jiyuan Lin <linjiyuan90@gmail.com>
2014-02-12 20:17:22 +0800

    test

--- leetcode/DivideTwoIntegers.cc
+++ leetcode/DivideTwoIntegers.cc
@@ -3,32 +3,29 @@ public:
   int divide(int dividend, int divisor) {
     // let k = dividend / divisor
     // Consider k's binary expression
-    
-    // note negative
-    // note INT_MAX
-    int s = (dividend > 0) ^ sign(divisor > 0) ? -1 : 1;
-    dividend = abs(dividend);
-    divisor = abs(divisor);
-
-    // Can I use shift?
-    int n = 1, d = divisor;
-    while (d < dividend) {
-      d <<= 1;
-      n <<= 1;
-    }
-    int k = 0;
-    while (dividend > 0 && d > 0) {
-      if (dividend >= d) {
-	dividend -= d;
-	k += n;
-      }
-      d >>= 1;
-      n >>= 1;
-    }
-    return k * s;
+    // Can't use shift, so solve it recursively.
+    // note negative, INT_MAX
+    int sign = (dividend > 0) ^ (divisor > 0) ? -1 : 1;
+    long long l_dividend = abs((long long)dividend);
+    long long l_divisor = abs((long long)divisor);
+    return divide(l_dividend, l_divisor) * sign;
   }
 
+private:
+  // substract it
+  long long divide(long long &dividend, long long divisor) {
+    if (divisor > dividend) {
+      return 0;
+    }
+    // try 2*divisor
+    long long ans = divide(dividend, divisor + divisor);
+    // need to double
+    // maybe pass in paramter will be more clear
+    ans += ans;
+    if (dividend >= divisor) {
+      ++ ans;
+      dividend -= divisor;
+    }
+    return ans;
+  }
 };
-
-
-// like kuaisumi
